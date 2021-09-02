42b9305f398a3eb416027f352b494a532c880b9f
brunomotadev <brunomotadev@gmail.com>
2021-09-02 16:54:59 -0300

    exercicio php e css

---
+++ exercicio.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+    <title>Exercicio</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualização do Exercicio</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+        </div>
+    </main>
+    <footer class="rodape">
+        C0D3R & ALUNOS <?= date('Y'); ?>
+    </footer>
+    
+</body>
+</html>
