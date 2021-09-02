bb472e49c68fcf70d601f8334bde06acd3d5089f
brunomotadev <brunomotadev@gmail.com>
2021-09-02 17:06:36 -0300

    navegacao exercicio

--- exercicio.php
+++ exercicio.php
@@ -6,13 +6,20 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/estilo.css">
+    <link rel="stylesheet" href="assets/css/exercicio.css">
     <title>Exercicio</title>
 </head>
-<body>
+<body class="exercicio">
     <header class="cabecalho">
         <h1>Curso PHP</h1>
         <h2>Visualização do Exercicio</h2>
     </header>
+
+    <nav class="navegacao">
+        <a href="#" class="verde">Sem formatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+    </nav>
+
     <main class="principal">
         <div class="conteudo">
         </div>
