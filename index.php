acaed42b9cdbc0621c076bf68b3da15cb0184dc7
brunomotadev <brunomotadev@gmail.com>
2021-09-03 13:39:28 -0300

    integração css e php

--- index.php
+++ index.php
@@ -25,6 +25,9 @@
                         <li>
                             <a href="exercicio.php?dir=basico&file=html">Integração HTML</a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=css">Integração CSS</a>
+                        </li>
                     </ul>
                 </div>
             </nav>
