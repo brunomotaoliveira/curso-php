d85a077a24125e4110834643584072788b566be2
brunomotadev <brunomotadev@gmail.com>
2021-09-03 13:27:16 -0300

    integração html e php

--- index.php
+++ index.php
@@ -22,6 +22,9 @@
                         <li>
                             <a href="exercicio.php?dir=basico&file=ola">Olá PHP</a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=html">Integração HTML</a>
+                        </li>
                     </ul>
                 </div>
             </nav>
