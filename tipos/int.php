887b4142f81168338ec92467f0b3144ace343fd7
brunomotadev <brunomotadev@gmail.com>
2021-09-03 16:24:23 -0300

    tipos em php - tipo inteiro

---
+++ tipos/int.php
@@ -0,0 +1,17 @@
+<div class="titulo">Tipo Inteiro</div>
+
+<?php 
+
+    echo 11;
+    echo '<br>';
+
+    var_dump(11);
+    echo '<br>';
+
+    echo PHP_INT_MAX, '<br>';
+    echo PHP_INT_MIN, '<br>';
+    echo 011, '<br>'; // base octal 0 a 7
+    echo 0b11, '<br>'; //representando numero em binario
+    echo 0x11; //hexadecimal
+
+?>
