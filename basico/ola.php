7a38aa0ed9592c15dc74e57b21a019c1cd88950d
brunomotadev <brunomotadev@gmail.com>
2021-09-02 21:05:05 -0300

    exemplos basicos do PHP

---
+++ basico/ola.php
@@ -0,0 +1,16 @@
+<div class="titulo">Olá PHP</div>
+
+
+<h2>Exemplo A</h2>
+<!-- utilizado mais para o backend da aplicação web -->
+<?php 
+    echo 'Olá <br>';
+    echo 'Mundo!';
+?>
+
+<h2>Exemplo B</h2>
+<!-- usado geralmente dentro de uma pagina -->
+<?= "Outra forma de me 'expressar'!" ?>
+
+<?php
+phpinfo();
