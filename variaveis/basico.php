68a5b17b38225f3d602e73768fe3a4d4c129498d
brunomotadev <brunomotadev@gmail.com>
2021-09-04 20:29:21 -0300

    basico de variaveis - parte 2

--- variaveis/basico.php
+++ variaveis/basico.php
@@ -26,4 +26,17 @@ echo '<br>' . $variavel;
 $variavel = "Agora sou uma string!";
 echo '<br>' . $variavel;
 
+
+// Nomes de variável
+$var = 'valida';
+$var2 = 'valida';
+$VAR3 = 'valida';
+$_var_4 = 'valida';
+$vâr5 = 'valida'; // evitar acentos
+// $6var = 'invalida';
+// $%var7 = 'invalida';
+// $var8% = 'invalida';
+
+echo '<br>';
+var_dump($_SERVER["HTTP_HOST"]);
 ?>
