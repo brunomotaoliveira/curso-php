692e430ae99e627f7fa4eda10c9ee0a652812324
brunomotadev <brunomotadev@gmail.com>
2021-09-04 21:03:18 -0300

    desafio resolvido

--- index.php
+++ index.php
@@ -71,6 +71,9 @@
                         <li>
                             <a href="exercicio.php?dir=variaveis&file=basico">Variáveis</a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=variaveis&file=desafio_equacao">Desafio Equação</a>
+                        </li>
                     </ul>
                 </div>
             </nav>
---
+++ variaveis/desafio_equacao.php
@@ -0,0 +1,18 @@
+<div class="titulo">Desafio Equação</div>
+
+<?php
+    $totalA = (((6 * (3 + 2))**2) / (3* 2));
+    $totalB = ((((1 - 5) * ( 2 - 7))/2)** 2);
+
+    $totalC = $totalA - $totalB;
+    $totalC = (($totalC ** 3) / (10 ** 3));
+
+    echo $totalA;
+    echo '<br>';
+    echo $totalB;
+    echo '<br>';
+    echo $totalC;
+
+
+
+?>
