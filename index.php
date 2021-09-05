0ecfa364a8d6849fbdaba976a6f4b441e0ca1c77
brunomotadev <brunomotadev@gmail.com>
2021-09-05 01:11:06 -0300

    desafio resolvido

--- index.php
+++ index.php
@@ -83,6 +83,9 @@
                         <li>
                             <a href="exercicio.php?dir=variaveis&file=variaveis_variaveis">Variáveis Variáveis</a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=variaveis&file=desafio_variaveis">Desafio Variáveis</a>
+                        </li>
                     </ul>
                 </div>
             </nav>
---
+++ variaveis/desafio_variaveis.php
@@ -0,0 +1,18 @@
+<div class="titulo">Desafio Variáveis</div>
+
+<?php
+$a = 'Nossa';
+$Nossa = 'Eu';
+$Eu = 'consegui';
+$consegui = 'responder';
+$responder = 'esse';
+$esse = 'desafio';
+
+echo "Nossa! Eu consegui responder esse desafio.";
+
+// resolução
+echo "<br>";
+echo "$a! {$$a} {$$$a} {$$$$a} {$$$$$a} {$$$$$$a}.";
+
+
+?>
