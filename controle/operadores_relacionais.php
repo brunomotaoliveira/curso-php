6b713275ed4a82b9b114fc62c34075204cf8f399
brunomotadev <brunomotadev@gmail.com>
2021-09-06 18:54:05 -0300

    operadores relacionais parte 2

--- controle/operadores_relacionais.php
+++ controle/operadores_relacionais.php
@@ -34,4 +34,23 @@
         echo "Terceira idade = $idade anos!";
     }
 
+    echo '<p>Spaceship</p><hr>';
+    var_dump(5 <=> 3); // <=>  : operador space ship
+    var_dump(50 <=> 50); // se forem iguais, retorna 0
+    var_dump(5 <=> 50); // se o primeiro for maior que o seguindo, retorna -1
+
+    echo '<p>Valores podem ser verdadeiro ou falso</p><hr>';
+    var_dump(!!5);
+    var_dump(!!0);
+    var_dump(!!"");
+    var_dump(!!"");
 ?>
+
+<style>
+    p {
+        margin-bottom: 0;
+    }
+    hr {
+        margin-top: 0;
+    }
+</style>
