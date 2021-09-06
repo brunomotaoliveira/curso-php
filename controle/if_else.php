fcafde83d69cefd664438f44b0d13d5b4ce8fbe1
brunomotadev <brunomotadev@gmail.com>
2021-09-06 16:47:41 -0300

    if else parte 2

--- controle/if_else.php
+++ controle/if_else.php
@@ -2,7 +2,38 @@
 
 <?php
     if(true){
-        echo "Serei impresso?";
-        echo "Serei impresso novamente?";
+        echo "Serei impresso?<br>";
     }
+
+    if(true) {
+        echo "Verdadero - Parte A<br>";
+        echo "Verdadeiro - Parte B<br>";
+    } else {
+        echo "Falso - Parte A<br>";
+        echo "Falso - Parte B<br>";
+    }
+
+    if(true) {
+        if(true) {
+            
+        }
+    }
+
+
+
+    if(false) {
+        echo "Passo A<br>";
+    } else if (true) {
+        echo "Passo B<br>";
+    } else if (true) {
+        echo "Passo C<br>";
+    } else if (true) {
+        echo "Passo D<br>";
+    } else if (true) {
+        echo "Passo E<br>";
+    } else {
+        echo "Ultimo Passo<br>";
+    }
+
+    echo "Fim<br>"
 ?>
