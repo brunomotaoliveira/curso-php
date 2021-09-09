d3d8b1bb91da096054ba48898622194ee4afe5b6
brunomotadev <brunomotadev@gmail.com>
2021-09-08 22:00:44 -0300

    desafio operadores logicos - minha solução

--- controle/desafio_operadores_logicos.php
+++ controle/desafio_operadores_logicos.php
@@ -17,7 +17,7 @@
     </div>
 
     <div>
-        <label for="t2">Trabalho 2(Quinta)</label>
+        <label for="t2">Trabalho 2(Quinta):</label>
         <select name="t2" id="t2">
             <option value="1">Executado</option>
             <option value="0">Não Executado</option>
@@ -25,3 +25,22 @@
     </div>
     <button>Executar</button>
 </form>
+
+<style>
+    button, select {
+        font-size: 1.8rem;
+    }
+</style>
+
+<?php
+    //echo $_POST['t1'];
+    //echo $_POST['t2'];
+
+    if(($_POST['t1'] === '0') || ($_POST['t2'] === '0')){
+        echo "Vamos comprar uma TV 32' e Sorvete";
+    } else if(($_POST['t1'] === '1') && ($_POST['t2'] === '1')){
+        echo "Vamos comprar uma TV 50' e Sorvete";
+    } else {
+        echo "Vamos ficar em casa que é mais saudável!";
+    }
+?>
