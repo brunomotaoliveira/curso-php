4b1188126f3bd2aa34ef6e6a43018100bc39a1a0
brunomotadev <brunomotadev@gmail.com>
2021-09-09 04:09:57 -0300

    desafio concluido

--- controle/desafio_switch.php
+++ controle/desafio_switch.php
@@ -23,33 +23,35 @@
     //  echo $_POST['conversao'];
     //  echo $_POST['param'];
 
-    $milha = 1.60934;
-    $metros = 1000;
+    const FATOR_KM_MILHA = 0.621371;
+    const FATOR_METRO_KM = 1000;
 
 
     switch($_POST['conversao']) {
         case 'km-milha':
-            $quantidadeDeMilhas = $_POST['param'] / $milha;
+            $quantidadeDeMilhas = $_POST['param'] * FATOR_KM_MILHA;
             $quantidadeDeMilhas = number_format($quantidadeDeMilhas,0, '.', '.'); // formata o numero/casas decimais
             echo $_POST['param'] . " quilômetros" . " tem $quantidadeDeMilhas milhas";
             break;
         
         case 'milha-km':
-            $quantidadeDeKm = $_POST['param'] * $milha;
+            $quantidadeDeKm = $_POST['param'] / FATOR_KM_MILHA;
             $quantidadeDeKm = number_format($quantidadeDeKm,0, '.', '.');
             echo $_POST['param'] . " milhas" . " tem $quantidadeDeKm quilômetros";
             break;
 
         case 'metro-km':
-            $quantidadeDeMetrosKm = $_POST['param'] / $metros;
+            $quantidadeDeMetrosKm = $_POST['param'] / FATOR_METRO_KM;
             $quantidadeDeMetrosKm = number_format($quantidadeDeMetrosKm,0, '.', '.');
             echo $_POST['param'] . " metros" . " tem $quantidadeDeMetrosKm quilômetros";
             break;
         
         case 'km-metro':
-            $quantidadeDeKmMetros = $_POST['param'] * $metros;
+            $quantidadeDeKmMetros = $_POST['param'] * FATOR_KM_MILHA;
             $quantidadeDeKmMetros = number_format($quantidadeDeKmMetros,0,'.', '.');
             echo $_POST['param'] . " quilômetros" . " tem $quantidadeDeKmMetros metros";
             break;
+        default:
+            $mensagem = "Nenhum valor calculado";
     }
 ?>
