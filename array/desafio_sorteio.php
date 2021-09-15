db6f7ae1ffc81c8da6efb1c47d39725db1ab3575
brunomotadev <brunomotadev@gmail.com>
2021-09-15 16:41:39 -0300

    solução do professor - desafio sorteio

--- array/desafio_sorteio.php
+++ array/desafio_sorteio.php
@@ -2,5 +2,14 @@
 
 <?php 
 $nomes = ["Elza", "Rapunzel", "Branca de neve", "Cinderela"];
-echo $nomes[array_rand($nomes)]; // array_rand : escolhe um elemento da array aleatoriamente
+//  echo $nomes[array_rand($nomes)]; // array_rand : escolhe um elemento da array aleatoriamente
+$index = array_rand($nomes);
+echo "<div center><h1>{$nomes[$index]}</h1></div>";
 ?>
+
+<style>
+    [center] {
+        display: flex;
+        justify-content: center;
+    }
+</style>
