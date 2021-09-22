74ae42de0a46a97bf22734792b3d260b5b1bc1f1
brunomotadev <brunomotadev@gmail.com>
2021-09-22 06:46:13 -0300

    usando o post

--- index.php
+++ index.php
@@ -153,6 +153,9 @@
                         <li>
                             <a href="exercicio.php?dir=array&file=get">$_GET</a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=array&file=post">$_POST</a>
+                        </li>
                     </ul>
                 </div>
             </nav>
