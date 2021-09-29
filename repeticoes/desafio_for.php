f37c1d3abff7359a9660b40d6269e9dafae66602
brunomotadev <brunomotadev@gmail.com>
2021-09-29 18:28:26 -0300

    adicionado o enunciado do desafio

--- repeticoes/desafio_for.php
+++ repeticoes/desafio_for.php
@@ -1,1 +1,15 @@
 <div class="titulo">Desafio For</div>
+
+<!-- 
+Usar o for, para gerar a seguinte saida
+#
+##
+###
+####
+#####
+
+São duas versões
+1) Pode usar incremento $i++
+
+2) não pode usar incremento $i++;
+-->
