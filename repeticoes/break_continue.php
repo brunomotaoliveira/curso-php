065a74283d6182d6fd733c21cb31ed041ad74bdb
brunomotadev <brunomotadev@gmail.com>
2021-10-01 12:32:55 -0300

    adicionado comentário sobre a execução da função

--- repeticoes/break_continue.php
+++ repeticoes/break_continue.php
@@ -6,7 +6,14 @@ for(;;) {
     echo "$cont <br>";
     $cont++;
     if($cont > 20) {
-        break;
+        break; // encerra o contador ao chegar em 20. Mostrando a mensagem ("Fim").
+    }
+}
+
+for(;;) {
+    $cont++;
+    if($cont % 2 === 1) { // só vai passar para as próximas linhas se o número for par
+        continue;
     }
 }
 
