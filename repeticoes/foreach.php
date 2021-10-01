a93d2c960780ce94d610d0a119666d5e6856e9ec
brunomotadev <brunomotadev@gmail.com>
2021-10-01 12:22:03 -0300

    Percorrendo for each dentro de for each

--- repeticoes/foreach.php
+++ repeticoes/foreach.php
@@ -18,4 +18,20 @@ foreach ($array as $valor) {
 foreach ($array as $indice => $valor) {
     echo "$indice => $valor <br>";
 }
+
+$matriz = [
+    ['a', 'e', 'i', 'o', 'u'],
+    ['b', 'c', 'd']
+];
+
+
+// for each dentro de for each
+foreach ($matriz as $linha) {
+    //echo "$linha <br>";
+
+    foreach($linha as $letra) {
+        echo "$letra";
+    }
+    echo "<br>";
+}
 ?>
