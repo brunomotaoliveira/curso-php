e2a60e7b5605f5746e59c834712b78dcaf027e5c
brunomotadev <brunomotadev@gmail.com>
2021-10-03 20:47:59 -0300

    desafio utilizando o foreach e continue

--- repeticoes/desafio_impressoes.php
+++ repeticoes/desafio_impressoes.php
@@ -22,4 +22,9 @@ for($i = 0; $i < count($array); $i++) {
     if($i % 2 === 1) continue;
     echo "{array[$i]} <br>";
 }
+
+foreach($array as $chave => $valor) {
+    if($chave % 2 !== 0) continue;
+    echo "$valor <br>";
+}
 ?>
