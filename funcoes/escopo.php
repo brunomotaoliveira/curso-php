905dd6425ddea804f50ea55dd7336032ebe6f590
brunomotadev <brunomotadev@gmail.com>
2021-10-17 15:59:50 -0300

    escopo da função - escopos diferentes

--- funcoes/escopo.php
+++ funcoes/escopo.php
@@ -7,4 +7,16 @@ function imprimirMensagens() {
 }
 
 imprimirMensagens();
+
+
+//escopo da função
+$variavel = 1;
+function trocaValor() {
+    $variavel = 2;
+    echo "Durante a função: $variavel <br>";
+}
+
+echo "Antes: $variavel <br>";
+trocaValor();
+echo "Depois: $variavel <br>";
 ?>
