416e4308ee69e0efe8fdd416f802e2fc5c6732a8
brunomotadev <brunomotadev@gmail.com>
2021-11-18 20:09:05 -0300

    função palindromo de forma dificil

--- funcoes/palindromo.php
+++ funcoes/palindromo.php
@@ -1,5 +1,18 @@
 <div class="titulo">Desafio Palindromo</div>
 
 <?php 
+    function palindromo($palavra) {
+        $ultimoIndice = strlen($palavra) - 1;
+        for($i = 0; $i <= $ultimoIndice; $i++) {
+            if($palavra[$i] !== $palavra[$ultimoIndice - $i]) {
+                return 'Não';
+            }
+        }
+        return 'Sim';
+    }
 
+    echo palindromo('arara') . '<br>';
+    echo palindromo('ana') . '<br>';
+    echo palindromo('abccba') . '<br>';
+    echo palindromo('bola') . '<br>';
 ?>
