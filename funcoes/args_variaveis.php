6d2fa5ba33551bc75eab6cf19fd9666d4dc57e6a
brunomotadev <brunomotadev@gmail.com>
2021-11-23 20:49:01 -0300

    função soma completa

--- funcoes/args_variaveis.php
+++ funcoes/args_variaveis.php
@@ -7,5 +7,12 @@
     }
 
     echo soma(14, 15, 6, 7, 8, 9) . '<br>';
+    echo soma(6, 5, 4) . '<br>';
+
+    function somaCompleta(...$numeros) {
+        print_r($numeros);
+    }
+
+    somaCompleta(1);
 
 ?>
