a4fee66f62cfd8b9dfd37723f4ea761933ac2ed1
brunomotadev <brunomotadev@gmail.com>
2021-12-05 22:26:51 -0300

    função somar1

--- funcoes/usando_tipos.php
+++ funcoes/usando_tipos.php
@@ -1,1 +1,12 @@
 <div class="titulo">Usando Tipos</div>
+
+<?php 
+function somar1($a, $b) {
+    echo "<span>Somando $a + $b = </span>";
+    return $a + $b;
+}
+
+echo somar1(1, 2) . '<br>';
+echo somar1(1.7, 2.5) . '<br>';
+echo somar1(1, '4dois') . '<br>';
+?>
