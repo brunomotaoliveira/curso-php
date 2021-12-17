a6cf70fbe9b234f8579649c5b11a6f17fe161e17
brunomotadev <brunomotadev@gmail.com>
2021-12-16 23:24:32 -0300

    is callable - exemplo 2

--- funcoes/closure_callable.php
+++ funcoes/closure_callable.php
@@ -7,6 +7,9 @@ $soma1 = function($a, $b) {
 };
 
 echo $soma1(2, 3) . '<br>';
-echo is_callable($soma1) ? 'Sim' : 'Não' . '<br>'
+echo (is_callable($soma1) ? 'Sim' : 'Não') . '<br>';
+
+$soma1 = 1;
+echo (is_callable($soma1) ? 'Sim' : 'Não') . '<br>';
 
 ?>
