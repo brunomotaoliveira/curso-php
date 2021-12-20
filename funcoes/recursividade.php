e1c5d7aa9174bdf90ab5f782c5f2ff97ea4a0aa3
brunomotadev <brunomotadev@gmail.com>
2021-12-20 18:40:27 -0300

    soma um ate funcao

--- funcoes/recursividade.php
+++ funcoes/recursividade.php
@@ -1,1 +1,15 @@
 <div class="titulo">Recursividade</div>
+
+<?php 
+
+function somaUmAte($numero) {
+    $soma = 0;
+    for(; $numero >= 1; $numero--) {
+        $soma += $numero;
+    }
+    return $soma;
+}
+
+echo somaUmAte(5) . '<br>';
+
+?>
