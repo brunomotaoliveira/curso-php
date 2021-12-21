24e5835f823e94a59ee8e93dbbc0a961cf6a8594
brunomotadev <brunomotadev@gmail.com>
2021-12-21 13:28:16 -0300

    usando o return no lugar do echo

--- classes_objetos/classe.php
+++ classes_objetos/classe.php
@@ -8,11 +8,20 @@ class Cliente {
     public $idade = 18;
 
     public function apresentar() {
-        echo "Nome: {$this->nome}<br>";
+        return "Nome: {$this->nome} Idade: {$this->idade}<br>";
     }
 }
 
 $c1 = new Cliente();
-$c1->apresentar(); // acessa o membro de um objeto criado
+$c1->nome = 'Ana Silva';
+$c1->idade = 28;
+// $c1->apresentar(); // acessa o membro de um objeto criado
+
+$c2 = new Cliente;
+$c2->nome = 'Bruno';
+$c2->idade = 30;
+
+echo $c1->apresentar(), '</br>';
+echo $c2->apresentar();
 
 ?>
