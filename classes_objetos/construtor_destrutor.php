01630feb4873b74480043ff4a28a6243dc9f3d95
brunomotadev <brunomotadev@gmail.com>
2021-12-21 16:50:55 -0300

    metodo construct

--- classes_objetos/construtor_destrutor.php
+++ classes_objetos/construtor_destrutor.php
@@ -3,13 +3,24 @@
 <?php
 
 class Pessoa {
+    //atributos
     public $nome;
     public $idade;
 
+    //metodo construtor
     function __construct($novoNome, $idade = 18)
     {
         echo 'Construtor invocado! <br>';
         $this->nome = $novoNome;
         $this->idade = $idade;        
+    }
+    //metodo destrutor
+    function __destruct()
+    {
+        echo 'E morreu!';
+    }
+
+    public function apresentar() {
+        
     }
 }
