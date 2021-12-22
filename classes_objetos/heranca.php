df752bb66b4af28c497d87ab7cd556f83a2a9f8d
brunomotadev <brunomotadev@gmail.com>
2021-12-22 19:58:00 -0300

    criado function apresentar

--- classes_objetos/heranca.php
+++ classes_objetos/heranca.php
@@ -14,6 +14,16 @@ class Pessoa {
     {
         $this->nome = $nome;
         $this->idade = $idade;
+        echo 'Pessoa Criada! <br>';
+    }
+
+    function __destruct()
+    {
+        echo 'Tchau!!';
+    }
+
+    public function apresentar() {
+        echo "{$this->nome}, {$this->idade} anos<br>";
     }
 }
 
