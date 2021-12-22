1f3dda25ffe58b4a4382f36c08e8babb1ab08ccb
brunomotadev <brunomotadev@gmail.com>
2021-12-22 19:55:44 -0300

    criado constructor e atributos

--- classes_objetos/heranca.php
+++ classes_objetos/heranca.php
@@ -1,1 +1,20 @@
 <div class="titulo">Herança</div>
+
+<?php 
+
+class Pessoa {
+
+    //atributos
+    public $nome;
+    public $idade;
+
+
+    //construtctor
+    function __construct($nome, $idade)
+    {
+        $this->nome = $nome;
+        $this->idade = $idade;
+    }
+}
+
+?>
