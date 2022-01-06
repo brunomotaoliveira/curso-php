23ad1feabb9a9ba4b61c4d7cc676cf33eaae4121
brunomotadev <brunomotadev@gmail.com>
2022-01-06 19:33:52 -0300

    funcao mostrar B

--- classes_objetos/visibilidade.php
+++ classes_objetos/visibilidade.php
@@ -12,6 +12,26 @@ class A {
         echo "a) Protegido = {$this->protegido}<br>";
         echo "a) Privado = {$this->privado}<br>";
     }
+
+    private function naoMostrar() { // não é possivel acessar diretamente
+        echo 'Não vou imprimir!';
+    }
 }
+
+$a = new A();
+$a->mostrarA();
+//$a->naoMostrar();
+
+echo "Fim!";
+
+class B extends A {
+    public function mostrarB() {
+        echo "Class B) Publico = {$this->publico}<br>";
+        echo "Class B) Protegido = {$this->protegido}<br>";
+    }
+}
+echo '<br>';
+$b = new B();
+$b->mostrarB();
 
 ?>
