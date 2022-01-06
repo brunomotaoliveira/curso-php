78f925e7bd60dd70dca0d2f49ccb8cb236134d30
brunomotadev <brunomotadev@gmail.com>
2022-01-06 19:26:17 -0300

    funcao mostrar A

--- classes_objetos/visibilidade.php
+++ classes_objetos/visibilidade.php
@@ -6,6 +6,12 @@ class A {
     public  $publico = 'Público';
     protected $protegido = 'Protegido';
     private $privado = 'Privado';
+
+    public function mostrarA() { //mostrar os atributos
+        echo "a) Publico = {$this->publico}<br>";
+        echo "a) Protegido = {$this->protegido}<br>";
+        echo "a) Privado = {$this->privado}<br>";
+    }
 }
 
 ?>
