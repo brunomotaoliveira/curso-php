214c28f5863c19af5bb244982918364f98b49e9f
brunomotadev <brunomotadev@gmail.com>
2022-01-07 11:00:41 -0300

    tentativa 2

--- classes_objetos/static.php
+++ classes_objetos/static.php
@@ -5,6 +5,16 @@
 class A {
     public $naoStatic = 'Variável de instância';
     public static $static = 'Variável de classe (estática)';
+
+    public function mostrarA() {
+        echo "Não estática = {$this->naoStatic}<br>";
+        //Tentativa 1
+        //echo "Estática = {$this->static}<br>";
+        echo "Estática = {self::$static}<br>";
+    }
 }
+
+$objetoA = new A();
+$objetoA->mostrarA();
 
 ?>
