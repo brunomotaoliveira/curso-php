eb35110d034f1d4a7e08a334db2347dbd521dea4
brunomotadev <brunomotadev@gmail.com>
2022-01-15 23:56:18 -0300

    fazendo teste com funcoes

---
+++ funcoes/teste_funcao.php
@@ -0,0 +1,19 @@
+<div class="titulo">Teste Interface</div>
+
+<?php 
+
+interface Animal {
+    function respirar();
+}
+
+class Cachorro implements Animal {
+    function respirar() {
+        echo "Irei usar oxigênio!<br>";
+    }
+}
+
+$animal1 = new Cachorro();
+$animal1->respirar();
+
+echo 'Fim!';
+?>
